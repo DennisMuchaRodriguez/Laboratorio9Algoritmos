466c0d7b1e50cb1655eccf58b5f0bcaf969c02b8
unknown <dengoku727@gmail.com>
2024-05-28 23:58:52 -0500

    PerdonemePorTodoProfe

--- Assets/Scripts/EnemyControlle.cs
+++ Assets/Scripts/EnemyControlle.cs
@@ -9,18 +9,24 @@ public class EnemyControlle : MonoBehaviour
     public float energy;
     public float maxEnergy;
     public float restTime;
+    public float visionRange = 5f; 
+    public float chaseEnergyDrain = 1f; 
     private bool isResting;
     private float restTimer;
     private int currentIndex;
     public GameObject objective;
     private float currentWeight;
+    private GameObject player; 
+    private bool isChasing;
+    private Vector2 originalPosition;
 
     public void InitializePath(Lista<GameObject> nodes)
     {
         pathNodes = nodes;
         currentIndex = 0;
         objective = pathNodes.Get(currentIndex);
-        currentWeight = 0; 
+        currentWeight = 0;
+        originalPosition = transform.position; 
     }
 
     void Start()
@@ -33,13 +39,15 @@ public class EnemyControlle : MonoBehaviour
         {
             objective = pathNodes.Get(currentIndex);
         }
+        player = GameObject.FindGameObjectWithTag("Player");
+        isChasing = false;
     }
 
     void Update()
     {
         if (isResting)
         {
-            restTimer = restTimer + Time.deltaTime;
+            restTimer += Time.deltaTime;
             if (restTimer >= restTime)
             {
                 isResting = false;
@@ -49,32 +57,52 @@ public class EnemyControlle : MonoBehaviour
         }
         else
         {
-            if (objective != null)
+            if (isChasing)
             {
-                transform.position = Vector2.SmoothDamp(transform.position, objective.transform.position, ref speedReference, 0.5f);
+                if (player != null)
+                {
+                    transform.position = Vector2.SmoothDamp(transform.position, player.transform.position, ref speedReference, 0.5f);
+                    energy -= chaseEnergyDrain * Time.deltaTime;
+
+                    if (energy <= 0)
+                    {
+                        isResting = true;
+                        isChasing = false;
+                    }
+
+                
+                    if (Vector2.Distance(transform.position, player.transform.position) > visionRange)
+                    {
+                        isChasing = false;
+                        objective = pathNodes.Get(currentIndex);
+                    }
+                }
+            }
+            else
+            {
+                if (objective != null)
+                {
+                    transform.position = Vector2.SmoothDamp(transform.position, objective.transform.position, ref speedReference, 0.5f);
+                }
             }
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Node" && !isResting)
+        if (collision.gameObject.tag == "Node" && !isResting && !isChasing)
         {
             if (collision.gameObject == objective)
             {
                 NodeController nodeController = collision.gameObject.GetComponent<NodeController>();
 
-                currentIndex = currentIndex + 1;
-                if (currentIndex >= pathNodes.Length)
-                {
-                    currentIndex = 0; 
-                }
+                currentIndex = (currentIndex + 1) % pathNodes.Length;
 
                 (NodeController nextNode, float weight) = nodeController.SelectRandomAdjacent();
                 objective = nextNode.gameObject;
-                currentWeight = weight; 
+                currentWeight = weight;
 
-                energy = energy - currentWeight; 
+                energy -= currentWeight;
 
                 if (energy <= 0)
                 {
@@ -82,5 +110,16 @@ public class EnemyControlle : MonoBehaviour
                 }
             }
         }
+
+        if (collision.gameObject.tag == "Player")
+        {
+            isChasing = true;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, visionRange);
     }
 }
